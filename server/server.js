045e47f99183a7fcca713243d42ad39b327d1a6c
John O’Brien <johnobrien8642@gmail.com>
2021-06-16 15:57:27 -0700

    build try 7

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ cronUserPostingHeat.start()
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/public'))
-  app.get('/', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
   })
 }
