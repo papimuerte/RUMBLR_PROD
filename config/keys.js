de582a20b0ee06d1c364c002e75e08c0a5cbfd23
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 12:14:12 -0700

    disregard needing keys_dev

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,12 @@
-import keys_prod from './keys_prod'
-import keys_dev from './keys_dev.js'
+// import keys_prod from './keys_prod'
+// import keys_dev from './keys_dev.js'
 
-export default process.env.NODE_ENV === 'development' ? keys_dev : keys_prod;
+// export default process.env.NODE_ENV === 'development' ? keys_dev : keys_prod;
+
+export default {
+  secretOrKey: process.env.SECRET_OR_KEY,
+  emailAuthToken: process.env.EMAIL_AUTH_TOKEN,
+  hostEmail: process.env.HOST_EMAIL,
+  hostPassword: process.env.HOST_PASSWORD,
+  mongoURL: process.env.MONGO_URL
+}
--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,12 @@
-import keys_prod from './keys_prod.js'
-import keys_dev from './keys_dev.js'
+// import keys_prod from './keys_prod.js'
+// import keys_dev from './keys_dev.js'
 
-export default process.env.NODE_ENV === 'development' ? keys_dev : keys_prod;
+// export default process.env.NODE_ENV === 'development' ? keys_dev : keys_prod;
+
+export default {
+  localIP: process.env.LOCAL_IP,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  bucket: process.env.BUCKET,
+  mongoURL: process.env.MONGO_URL
+}
