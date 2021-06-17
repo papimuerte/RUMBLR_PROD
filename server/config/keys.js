ab940fb7afbc4945abfc14105e68b786cb33861a
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 08:50:51 -0700

    rearrange keys file

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,9 @@
+import keys_prod from './keys_prod.js'
+import keys_dev from './keys_dev.js'
+
+
 if (process.env.NODE_ENV === 'production') {
-  import keys_prod from './keys_prod.js'
+  export default keys_prod;
 } else {
-  import keys_dev from './keys_dev.js'
+  export default keys_dev;
 }
