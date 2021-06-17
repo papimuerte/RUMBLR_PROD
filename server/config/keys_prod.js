e14b84f0e8103adfea31101aef6115ff401cde21
John O’Brien <johnobrien8642@gmail.com>
2021-06-16 19:05:37 -0700

    update keys prod in server

--- server/config/keys_prod.js
+++ server/config/keys_prod.js
@@ -1,9 +1,7 @@
-const keys = {
+export default keys_prod = {
   localIP: process.env.LOCAL_IP,
   accessKeyId: process.env.ACCESS_KEY_ID,
   secretAccessKey: process.env.SECRET_ACCESS_KEY,
   bucket: process.env.BUCKET,
-  mongoURL: process.env.MONGO_ATLAS_URL
+  mongoURL: process.env.MONGO_URL
 }
-
-export default keys;
