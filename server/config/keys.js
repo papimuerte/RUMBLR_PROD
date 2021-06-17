1b0b60fc3dedfd77df83cf3106feb13a747f107c
John O’Brien <johnobrien8642@gmail.com>
2021-06-17 09:35:51 -0700

    continue adding module.exports to keys file

--- server/config/keys.js
+++ server/config/keys.js
@@ -5,5 +5,5 @@ import keys_dev from './keys_dev.js'
 if (process.env.NODE_ENV === 'production') {
   module.exports = keys_prod;
 } else {
-  export default keys_dev;
+  module.exports = keys_dev;
 }
